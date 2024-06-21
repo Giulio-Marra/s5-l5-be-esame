cc91ee44b7e042fe96bd38ba5d11d970e8c54703
Giulio-Marra <giuliomarramail@gmail.com>
2024-06-21 09:11:01 +0200

    start_project

---
+++ s5-l5-be-esame/src/main/java/giulio_marra/s5_l5_be_esame/S5L5BeEsameApplication.java
@@ -0,0 +1,13 @@
+package giulio_marra.s5_l5_be_esame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5L5BeEsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5L5BeEsameApplication.class, args);
+	}
+
+}
---
+++ s5-l5-be-esame/src/test/java/giulio_marra/s5_l5_be_esame/S5L5BeEsameApplicationTests.java
@@ -0,0 +1,13 @@
+package giulio_marra.s5_l5_be_esame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5L5BeEsameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
